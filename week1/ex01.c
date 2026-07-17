c4bc084f17e635c0a6021b8ab0673bc6f4482ae6
Kantinan <156271656+pondsj62@users.noreply.github.com>
2026-07-17 02:04:27 +0000

    week1 complete

--- week1/ex01.c
+++ week1/ex01.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    printf("Programming  : %3c\n",'A');
+    printf("Programming  : %c\n",'A');
+    printf("Drawing      : %s\n","B+");
+    printf("Mechanics    : %s\n","B+");
+    printf("Math         : %s\n","B");
+    printf("GPS          : %.1f\n",3.5);
     return 0;
 
 }
